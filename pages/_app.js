8bff5d739705cad2ea87eee37438cdd967b968ef
riya <riyawani26@gmail.com>
2023-06-01 17:58:13 +0530

    favicon-changes

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import LoadingScreen from '../components/LoadingScreen';
 import "../styles/globals.css"
+import Head from "next/head"
+
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
   const [isMounting, setIsMounting] = useState(true);
@@ -33,6 +35,15 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png "/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+      <meta name="msapplication-TileColor" content="#da532c"/>
+      <meta name="theme-color" content="#252525"/>
+    </Head>
       {loading && <LoadingScreen />}
       <Component {...pageProps} />
     </>

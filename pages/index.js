ca46cbd2b1c7f2cef706e9f7cccc6294a95303fb
riya <riyawani26@gmail.com>
2023-07-04 11:19:25 +0530

    changed comments

--- pages/index.js
+++ pages/index.js
@@ -71,46 +71,6 @@ export default function App() {
           <div className={styles.centerCards}>
             <section className={styles.aboutCards}>
               {aboutUs.map(createEntry)}
-              {/* <CardUs
-               n="aunty" 
-               t="Mustafa Trunkwala"
-               d="Full Stack Web Developer"
-               lg="https://github.com/TechyMT"
-               ll="https://www.linkedin.com/in/mustafa-trunkwala-49249a22a/"
-               li="https://www.instagram.com/nothim_7856/"
-               />
-              <CardUs 
-                n="riya"
-                t="Riya Wani"
-                d="Frontend Developer,UI/UX designer"
-                lg="https://github.com/Riyaa2610"
-                ll="https://www.linkedin.com/in/riya-wani-114672229/"
-                li="https://www.instagram.com/postcardsbyriya/"
-              />
-              <CardUs
-                n="rhea1" 
-                t="Rhea Shah" 
-                d="UI/UX designer"
-                lg="https://github.com/Rheaa21"
-                ll="https://www.linkedin.com/in/rhea-shah-1b7a95253/"
-                li="https://www.instagram.com/rheashah21/"
-                />
-              <CardUs 
-                n="surana1" 
-                t="Ameya Surana"
-                d="App Developer"
-                lg="https://github.com/FireFeast7"
-                ll="https://www.linkedin.com/in/ameya-surana-b8a49b22a/"
-                li="https://www.instagram.com/surana_ameya/"
-              />
-              <CardUs 
-                n="amogh"   
-                t="Amogh Thakurdesai"
-                d="App Developer"
-                lg="https://github.com/AmoghThakurdesai"
-                ll="https://www.linkedin.com/in/amogh-thakurdesai-5a3456149/"
-                li="https://www.instagram.com/amoghptd/"
-              /> */}
             </section>
           </div>
         </div>
